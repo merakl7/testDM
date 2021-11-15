0b9c36e24c25f934905002c3acface9eb20d64c4
merakl7 <58310401+merakl7@users.noreply.github.com>
2021-11-15 13:26:11 +0300

    Add files via upload

---
+++ testDM/how.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title >Главная страница</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+		<a class="navbar-brand" href = "how.php">Главная страница</a>
+		<a class="navbar-brand"  href="news.php">Новости <span class="sr-only">(current)</span></a>
+		<a class="navbar-brand" href="index.php">Поддержка</a>
+
+
+</head>
+<body>
+
+  <style>
+    body {
+      background: #FFF5EE;
+      color: #000000;
+    }
+  </style>
+  
+	<style>
+
+  
+
+.center { 
+
+ top: 800%;
+   border: thick double  ;
+  left: 15%; 
+  font-size: 80%;
+  width: 450px; 
+  height: 180px; 
+  position: absolute; 
+  margin-top: -225px; 
+  margin-left: -225px;
+
+  background: #E6E6FA;
+  color: #000000;}
+
+.center2 {  
+
+ top: 800%;
+   border: thick double ;
+  left: 30%; 
+  font-size: 80%;
+  width: 450px; 
+  height: 180px; 
+  position: absolute; 
+  margin-top: -225px; 
+;
+
+  background: #E6E6FA;
+  color: #000000;}
+
+  .center3 {  
+
+ top: 800%;
+   border: thick double ;
+  left: 57%; 
+  font-size: 80%;
+  width: 450px; 
+  height: 180px; 
+  position: absolute; 
+  margin-top: -225px; 
+  ;
+
+  background: #E6E6FA;
+  color: #000000;}
+
+</style>
+
+<div id = "article", align="center" class="center" >
+  
+  <article class="news">
+    <header>
+      <h3> Ветер </h3>
+      <time datetime="2021-11-12">ноябрь 2021</time>
+    </header>
+    <section class="text">
+      <p>
+        Душа моя озарена неземной радостью, как эти чудесные весенние утра, которыми я наслаждаюсь от всего сердца. 
+      </p>
+    </section>
+  </article>
+</div>
+
+<div id = "article", align="center" class="center2" >
+  
+  <article class="news">
+    <header>
+      <h3> Прототип </h3>
+      <time datetime="2021-11-12">ноябрь 2021</time>
+    </header>
+    <section class="text">
+      <p>
+        С другой стороны, высококачественный прототип будущего проекта требует анализа укрепления моральных ценностей. Равным образом, базовый вектор развития не даёт нам иного выбора, кроме определения распределения внутренних резервов и ресурсов.
+      </p>
+    </section>
+  </article>
+</div>
+<div id = "article", align="center" class="center3" >
+  
+  <article class="news">
+    <header>
+      <h3> Диограммы </h3>
+      <time datetime="2021-11-12">ноябрь 2021</time>
+    </header>
+    <section class="text">
+      <p>
+        Сложно сказать, почему диаграммы связей освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, представлены в исключительно положительном свете. Значимость этих проблем настолько очевидна, что высококачественный прототип.
+      </p>
+    </section>
+  </article>
+</div> 
+
+ </body>
+    </form>
+  </div>
+
+</html>
---
+++ testDM/news.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title >новости</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+		<a class="navbar-brand" href = "how.php">Главная страница</a>
+		<a class="navbar-brand"  href="news.php">Новости <span class="sr-only">(current)</span></a>
+		<a class="navbar-brand" href="index.php">Поддержка</a>
+
+
+</head>
+<body>
+
+<style>
+		body {
+			background: #FFF5EE;
+			color: #000000;
+		}
+	</style>
+
+<style>
+
+
+
+
+.center {	
+
+ top: 800%;
+   border: thick double  ;
+  left: 15%; 
+  font-size: 80%;
+  width: 450px; 
+  height: 180px; 
+  position: absolute; 
+  margin-top: -225px; 
+  margin-left: -225px;
+
+  background: #E6E6FA;
+  color: #000000;}
+
+.center2 {	
+
+ top: 800%;
+   border: thick double ;
+  left: 30%; 
+  font-size: 80%;
+  width: 450px; 
+  height: 180px; 
+  position: absolute; 
+  margin-top: -225px; 
+;
+
+  background: #E6E6FA;
+  color: #000000;}
+
+  .center3 {	
+
+ top: 800%;
+   border: thick double ;
+  left: 57%; 
+  font-size: 80%;
+  width: 450px; 
+  height: 180px; 
+  position: absolute; 
+  margin-top: -225px; 
+  ;
+
+  background: #E6E6FA;
+  color: #000000;}
+
+  .center4 {	
+
+ top: 800%;
+   border: thick double ;
+  left: 30%; 
+  font-size: 80%;
+  width: 450px; 
+  height: 180px; 
+  position: absolute; 
+  margin-top: -25px; 
+  ;
+
+  background: #E6E6FA;
+  color: #000000;}
+
+  .center5 {	
+
+ top: 800%;
+   border: thick double ;
+  left: 15%; 
+  font-size: 80%;
+  width: 450px; 
+  height: 180px; 
+  position: absolute; 
+  margin-top: -25px; 
+  margin-left: -225px;
+  
+  background: #E6E6FA;
+  color: #000000;}
+
+  
+</style>
+
+
+
+<div id = "article", align="center" class="center" >
+	
+	<article class="news">
+		<header>
+			<h3> Ветер </h3>
+			<time datetime="2021-11-12">ноябрь 2021</time>
+		</header>
+		<section class="text">
+			<p>
+				Душа моя озарена неземной радостью, как эти чудесные весенние утра, которыми я наслаждаюсь от всего сердца. 
+			</p>
+		</section>
+	</article>
+</div>
+
+<div id = "article", align="center" class="center2" >
+	
+	<article class="news">
+		<header>
+			<h3> Прототип </h3>
+			<time datetime="2021-11-12">ноябрь 2021</time>
+		</header>
+		<section class="text">
+			<p>
+				С другой стороны, высококачественный прототип будущего проекта требует анализа укрепления моральных ценностей. Равным образом, базовый вектор развития не даёт нам иного выбора, кроме определения распределения внутренних резервов и ресурсов.
+			</p>
+		</section>
+	</article>
+</div>
+<div id = "article", align="center" class="center3" >
+	
+	<article class="news">
+		<header>
+			<h3> Диограммы </h3>
+			<time datetime="2021-11-12">ноябрь 2021</time>
+		</header>
+		<section class="text">
+			<p>
+				Сложно сказать, почему диаграммы связей освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, представлены в исключительно положительном свете. Значимость этих проблем настолько очевидна, что высококачественный прототип.
+			</p>
+		</section>
+	</article>
+</div>
+<div id = "article", align="center" class="center4" >
+	
+	<article class="news">
+		<header>
+			<h3> Гражданин </h3>
+			<time datetime="2021-11-12">ноябрь 2021</time>
+		</header>
+		<section class="text">
+			<p>
+				Приятно, граждане, наблюдать, как действия представителей оппозиции освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, объективно рассмотрены соответствующими инстанциями. 
+			</p>
+		</section>
+	</article>
+</div>
+<div id = "article", align="center" class="center5" >
+	
+	<article class="news">
+		<header>
+			<h3> Рост </h3>
+			<time datetime="2021-11-12">ноябрь 2021</time>
+		</header>
+		<section class="text">
+			<p>
+				Значимость этих проблем настолько очевидна, что постоянный количественный рост и сфера нашей активности требует от нас анализа существующих финансовых и административных условий. Кстати, реплицированные с зарубежных источников. 
+			</p>
+		</section>
+	</article>
+</div>
+
+
+ </body>
+</html>
+
